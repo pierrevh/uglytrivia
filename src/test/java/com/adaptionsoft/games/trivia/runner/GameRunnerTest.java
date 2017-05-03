f24e6a8a15112349167ff2e8785bfce5ab32eb5a
Pierre van Hees <pvhees@xs4all.nl>
2017-05-03 15:46:07 +0200

    using golden master for testing

--- src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -1,6 +1,5 @@
 package com.adaptionsoft.games.trivia.runner;
 
-import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,15 +8,11 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
 
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
-import static org.junit.matchers.JUnitMatchers.hasItem;
-import static org.junit.matchers.JUnitMatchers.hasItems;
 
 public class GameRunnerTest {
-
 
     private PrintStream productionSystemOut;
 
@@ -31,27 +26,100 @@ public class GameRunnerTest {
         System.setOut(productionSystemOut);
     }
 
-    @Test
-    public void canRunFromMain() throws Exception {
-        GameRunner.main(new String[] {});
-        assertTrue(true);
-    }
+    private static final String goldenMasterAlwaysRoll4AndNoPenaltyBox =
+        "Pierre was added\n" +
+        "They are player number 1\n" +
+        "Henkie was added\n" +
+        "They are player number 2\n" +
+        "Pierre is the current player\n" +
+        "They have rolled a 4\n" +
+        "Pierre's new location is 4\n" +
+        "The category is Pop\n" +
+        "Pop Question 0\n" +
+        "Answer was correct!!!!\n" +
+        "Pierre now has 1 Gold Coins.\n" +
+        "Henkie is the current player\n" +
+        "They have rolled a 4\n" +
+        "Henkie's new location is 4\n" +
+        "The category is Pop\n" +
+        "Pop Question 1\n" +
+        "Answer was correct!!!!\n" +
+        "Henkie now has 1 Gold Coins.\n" +
+        "Pierre is the current player\n" +
+        "They have rolled a 4\n" +
+        "Pierre's new location is 8\n" +
+        "The category is Pop\n" +
+        "Pop Question 2\n" +
+        "Answer was correct!!!!\n" +
+        "Pierre now has 2 Gold Coins.\n" +
+        "Henkie is the current player\n" +
+        "They have rolled a 4\n" +
+        "Henkie's new location is 8\n" +
+        "The category is Pop\n" +
+        "Pop Question 3\n" +
+        "Answer was correct!!!!\n" +
+        "Henkie now has 2 Gold Coins.\n" +
+        "Pierre is the current player\n" +
+        "They have rolled a 4\n" +
+        "Pierre's new location is 0\n" +
+        "The category is Pop\n" +
+        "Pop Question 4\n" +
+        "Answer was correct!!!!\n" +
+        "Pierre now has 3 Gold Coins.\n" +
+        "Henkie is the current player\n" +
+        "They have rolled a 4\n" +
+        "Henkie's new location is 0\n" +
+        "The category is Pop\n" +
+        "Pop Question 5\n" +
+        "Answer was correct!!!!\n" +
+        "Henkie now has 3 Gold Coins.\n" +
+        "Pierre is the current player\n" +
+        "They have rolled a 4\n" +
+        "Pierre's new location is 4\n" +
+        "The category is Pop\n" +
+        "Pop Question 6\n" +
+        "Answer was correct!!!!\n" +
+        "Pierre now has 4 Gold Coins.\n" +
+        "Henkie is the current player\n" +
+        "They have rolled a 4\n" +
+        "Henkie's new location is 4\n" +
+        "The category is Pop\n" +
+        "Pop Question 7\n" +
+        "Answer was correct!!!!\n" +
+        "Henkie now has 4 Gold Coins.\n" +
+        "Pierre is the current player\n" +
+        "They have rolled a 4\n" +
+        "Pierre's new location is 8\n" +
+        "The category is Pop\n" +
+        "Pop Question 8\n" +
+        "Answer was correct!!!!\n" +
+        "Pierre now has 5 Gold Coins.\n" +
+        "Henkie is the current player\n" +
+        "They have rolled a 4\n" +
+        "Henkie's new location is 8\n" +
+        "The category is Pop\n" +
+        "Pop Question 9\n" +
+        "Answer was correct!!!!\n" +
+        "Henkie now has 5 Gold Coins.\n" +
+        "Pierre is the current player\n" +
+        "They have rolled a 4\n" +
+        "Pierre's new location is 0\n" +
+        "The category is Pop\n" +
+        "Pop Question 10\n" +
+        "Answer was correct!!!!\n" +
+        "Pierre now has 6 Gold Coins.\n";
 
-    private List<String> goldenMaster = Arrays.asList(""); //TODO fill this and use in test below
+    private List<String> goldenMasterAsList = Arrays.asList(goldenMasterAlwaysRoll4AndNoPenaltyBox);
 
     @Test
-    public void canRunWithFixedControls() throws Exception {
+    public void showsGoldenMasterOutputWhenUsingFixedControlsForGoldenMaster() throws Exception {
         ByteArrayOutputStream canvas = new ByteArrayOutputStream();
         System.setOut(new PrintStream(canvas));
 
         GameRunner.run(Arrays.asList("Pierre", "Henkie"), new FixedNumberGenerator());
         List<String> lines = lines(canvas.toString("UTF-8"));
-        // using Golden Master sort of
-        assertThat(lines, hasItem("Pierre was added"));
-        assertThat(lines, hasItem("They are player number 1"));
-        assertThat(lines, hasItem("Henkie was added"));
-        assertThat(lines, hasItem("They are player number 2"));
-        assertThat(lines, hasItem("Pierre now has 6 Gold Coins."));
+
+        assertEquals(lines(goldenMasterAlwaysRoll4AndNoPenaltyBox), lines);
         assertThat(lines, not(hasItem("They are player number 3")));
     }
 
